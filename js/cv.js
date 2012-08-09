1da8e23e93f00fe6555924152f4390365a025198
dmitry-p <MitrickX@mail.ru>
2012-08-09 21:43:03 +0400

    floating toolbar

--- js/cv.js
+++ js/cv.js
@@ -1,17 +1,45 @@
 $(function() {
 
-$('.collapsible .collapse-handler').click(function() {
-    var self = $(this),
-        li = self.parents('li:first'),
-        content = li.find('.collapse-content');
+makeCollapsibleCV();
+makeFloatingToolbar();
 
-    if (self.hasClass('darr')) {
-        self.removeClass('darr').addClass('rarr');
-        content.hide();
-    } else {
-        self.removeClass('rarr').addClass('darr');
-        content.show();
-    }
-});
+function makeCollapsibleCV() {
+    $('.collapsible .collapse-handler').click(function() {
+        var self = $(this),
+            li = self.parents('li:first'),
+            content = li.find('.collapse-content');
+
+        if (self.hasClass('darr')) {
+            self.removeClass('darr').addClass('rarr');
+            content.hide();
+        } else {
+            self.removeClass('rarr').addClass('darr');
+            content.show();
+        }
+    });
+}
+
+function makeFloatingToolbar() {
+    var container = $('.body'),
+        top_offset = container.position()['top'];
+    function handler(e) {
+        var toolbar = $('.toolbar');
+        if (!toolbar.length) {
+            return;
+        }
+        if (typeof handler.top === 'undefined') {
+            handler.top = toolbar.position()['top'];
+        }
+        if ($(document).scrollTop() - top_offset > handler.top) {
+            toolbar.addClass('fixed');
+        } else {
+            toolbar.removeClass('fixed');
+        }
+    };
+    // check right now (maybe document is scrolled)
+    handler();
+    // bind handler
+    $(document).bind('scroll', handler);
+}
 
 });
